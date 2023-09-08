f2f7427242521fcb85824e6ba1c87879daac6a28
AiWaldoh <kevin@gptcoder.pro>
2023-09-08 11:40:55 -0300

    moved api key to secrets.toml

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 
-    ###password section###
-    # streamlit_app.py
+###password section###
+# streamlit_app.py
+
 
 def check_password():
     """Returns `True` if the user had the correct password."""
@@ -43,23 +45,28 @@ def check_password():
         # Password correct.
         return True
 
+
 if check_password():
-    os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+    os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
     loader = DirectoryLoader("./theorie/", glob="*.txt")
     docs = loader.load_and_split()
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     texts = text_splitter.split_documents(docs)
     embeddings = OpenAIEmbeddings()
-    vectorstore = Chroma.from_documents(documents=texts, embedding=embeddings, persist_directory="./chromadb")
+    vectorstore = Chroma.from_documents(
+        documents=texts, embedding=embeddings, persist_directory="./chromadb"
+    )
     vectorstore.persist()
-    retriever = vectorstore.as_retriever(search_kwargs={"k":4})
+    retriever = vectorstore.as_retriever(search_kwargs={"k": 4})
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
     llm = ChatOpenAI(model_name="gpt-3.5-turbo-16k", temperature=0.2, verbose=True)
-    qa = ConversationalRetrievalChain.from_llm(llm, retriever, memory=memory, verbose=True)
+    qa = ConversationalRetrievalChain.from_llm(
+        llm, retriever, memory=memory, verbose=True
+    )
 
     st.title("CCNB SYST1046 Chatbot")
 
-    openai.api_key = os.getenv("OPENAI_API_KEY")
+    openai.api_key = st.secrets["OPENAI_API_KEY"]
 
     if "openai_model" not in st.session_state:
         st.session_state["openai_model"] = "gpt-3.5-turbo-16k"
@@ -79,6 +86,8 @@ if check_password():
         with st.chat_message("assistant"):
             message_placeholder = st.empty()
             full_response = ""
-            response =  qa({"question": prompt})
+            response = qa({"question": prompt})
             message_placeholder.markdown(response["answer"])
-        st.session_state.messages.append({"role": "assistant", "content": response["answer"]})
+        st.session_state.messages.append(
+            {"role": "assistant", "content": response["answer"]}
+        )
