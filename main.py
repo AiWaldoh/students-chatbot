be3222fea16bfa701111b11f679def8bcb68921f
AiWaldoh <kevin@gptcoder.pro>
2023-09-08 21:58:56 -0300

    refactored and using QA chain without memory for now

--- main.py
+++ main.py
@@ -12,11 +12,13 @@ from langchain.vectorstores import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import DirectoryLoader
 from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
+from langchain.chains import RetrievalQA
+from langchain import PromptTemplate
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
 
 
 ###password section###
@@ -48,29 +50,62 @@ def check_password():
         # Password correct.
         return True
 
+@st.cache_resource(show_spinner=False)
+def get_db():
+    with st.spinner(text="Chargement et indexation des documents du cours – patientez s'il vous plaît!"):
+        os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+        # openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+        persist_directory="./chromadb"
+        loader = DirectoryLoader("./theorie/", glob="*.txt")
+        docs = loader.load_and_split()
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        texts = text_splitter.split_documents(docs)
+        embeddings = OpenAIEmbeddings()
+        vectordb = Chroma.from_documents(
+            documents=texts, embedding=embeddings, persist_directory=persist_directory
+        )
+        vectordb.persist()
+        return vectordb
+
+def init_retriever(vectorstore):
+    retriever = vectorstore.as_retriever(search_kwargs={"k": 4})
+    return retriever
+def init_memory():
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    return memory
+
+def init_model():
+    model = ChatOpenAI(model_name="gpt-3.5-turbo-16k", temperature=0.2, verbose=True)
+    return model
+
+def init_prompt():
+    template = """Vous êtes un enseignant du college communautaire du Nouveau-Brunswick. Vos réponses sont élaborées et détaillées.
+        Terminez toujours vos réponses par "Merci pour votre question!"
+        {context}
+        Question: {question}
+        Réponse:"""
+    return PromptTemplate.from_template(template)
+
+
+def init_qa(llm, retriever, QA_CHAIN_PROMPT):
+    return RetrievalQA.from_chain_type(
+        llm,
+        retriever=vectorstore.as_retriever(), 
+        chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
+        verbose=True
+    )
 
 if check_password():
-    os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
-    loader = DirectoryLoader("./theorie/", glob="*.txt")
-    docs = loader.load_and_split()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    texts = text_splitter.split_documents(docs)
-    embeddings = OpenAIEmbeddings()
-    vectorstore = Chroma.from_documents(
-        documents=texts, embedding=embeddings, persist_directory="./chromadb"
-    )
-    vectorstore.persist()
-    retriever = vectorstore.as_retriever(search_kwargs={"k": 4})
-    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-    llm = ChatOpenAI(model_name="gpt-3.5-turbo-16k", temperature=0.2, verbose=True)
-    qa = ConversationalRetrievalChain.from_llm(
-        llm, retriever, memory=memory, verbose=True
-    )
-
+    vectorstore = get_db()
+    retriever = init_retriever(vectorstore)
+    memory = init_memory()
+    llm = init_model()
+    QA_CHAIN_PROMPT = init_prompt()
+    qa = init_qa(llm, retriever, QA_CHAIN_PROMPT)
+    
     st.title("CCNB SYST1046 Chatbot")
-
-    openai.api_key = st.secrets["OPENAI_API_KEY"]
-
+   
     if "openai_model" not in st.session_state:
         st.session_state["openai_model"] = "gpt-3.5-turbo-16k"
 
@@ -91,8 +126,8 @@ if check_password():
                 
                 message_placeholder = st.empty()
                 full_response = ""
-                response = qa({"question": prompt})
-                message_placeholder.markdown(response["answer"])
+                response = qa({"query": prompt})
+                message_placeholder.markdown(response["result"])
             st.session_state.messages.append(
-                {"role": "assistant", "content": response["answer"]}
+                {"role": "assistant", "content": response["result"]}
             )
